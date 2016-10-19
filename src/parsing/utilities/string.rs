7f98488540df187e34784fdf4dd7cf71839836e1
Austin <austinrf2918@yahoo.com>
2016-10-19 00:26:49 -0400

    Final refactoring of string utilities for now.

--- src/parsing/utilities/string.rs
+++ src/parsing/utilities/string.rs
@@ -21,30 +21,33 @@ fn captures_exist(captures: Option<Captures>) -> bool {
 pub fn has_notation_character( snp: StringNotationPattern, c: char, s: &str) -> bool {
     match snp {
         StringNotationPattern::First => {
-            let f_regex: Regex = Regex::new(("^".to_string() + c.to_string().as_str() + ".*$").as_str()).unwrap();
+            let f_regex: Regex = Regex::new((format!("^{}.*$", c)).as_str()).unwrap();
             captures_exist(f_regex.captures(s))
-        },
+        }
+
         StringNotationPattern::Last => {
-            let l_regex: Regex = Regex::new(("^".to_string() + ".*" + c.to_string().as_str() + "$").as_str()).unwrap();
+            let l_regex: Regex = Regex::new((format!("^.*{}$", c)).as_str()).unwrap();
             captures_exist(l_regex.captures(s))
-        }, StringNotationPattern::External => {
+        }
+
+        StringNotationPattern::External => {
             has_notation_character(StringNotationPattern::First, c, s) ||
             has_notation_character(StringNotationPattern::Last, c, s)
                 
-        }, StringNotationPattern::Internal => {
-            let mut num = 0;
-            let mut flag = false;
-            for i in s.chars() {
-                if num != 0 && num != s.len() - 1 {
-                    if i == c {
-                        flag = true
-                    } 
+        }
+
+        StringNotationPattern::Internal => {
+            for (num, letter) in s.chars().enumerate() {
+                if num != 0 && num != s.len() - 1 && letter == c {
+                    return true
                 }
-                num = num + 1;
             }
-            flag
-        }, StringNotationPattern::Contains => {
-            let i_regex: Regex = Regex::new(("^".to_string() + ".*" + c.to_string().as_str() + ".*" + "$").as_str()).unwrap();
+
+            false
+        }
+
+        StringNotationPattern::Contains => {
+            let i_regex: Regex = Regex::new((format!("^.*{}.*$", c)).as_str()).unwrap();
             captures_exist(i_regex.captures(s))
         }
     }
