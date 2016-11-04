1708c458476d4c1a3bdc9e27c4f7962b1b007f90
Austin Fell <austinrf2918@yahoo.com>
2016-11-04 16:04:54 -0400

    Made equality check memory safe.

--- src/expression/structure.rs
+++ src/expression/structure.rs
@@ -24,8 +24,41 @@ impl Eq for SimplexPointer {
 
 impl PartialEq for SimplexPointer{
     fn eq(&self, other: &Self) -> bool {
-        // Deadlock risk.
-        (*(self.internal_data.lock().unwrap().to_string())) == (*(other.internal_data.lock().unwrap().to_string()))
+        // Note that I believe that this schema is in general capable of deadlocking:
+        // here is the rational behind it:
+        // ExpressionA: SimplexPointer , ExpressionB: SimplexPointer
+        // 
+        // Let's say that ExpressionA checks for equality on ExpressionB on one thread,
+        // and that the opposite case occurs on another thread.
+        //
+        // Thread1:                Thread2:
+        // ExpressionA.lock()      ExpressionB.lock()
+        // ExpressionB.lock()      ExpressionA.lock()
+        // waiting on B            waiting on A
+        //
+        // For this reason, it may be a better idea instead of using a Mutex to use a
+        // RWCell.
+
+        let lock_lhs = self.internal_data.lock();
+        let lock_rhs = other.internal_data.lock();
+
+        match (lock_lhs, lock_rhs) {
+            (Ok(guard_lhs), Ok(guard_rhs)) => {
+                *guard_lhs.to_string() == *guard_rhs.to_string()
+            }
+
+            (Err(poisoned_lhs), Ok(guard_rhs)) => {
+                *(poisoned_lhs.into_inner()).to_string() == *guard_rhs.to_string()
+            }
+
+            (Ok(guard_lhs), Err(poisoned_rhs)) => {
+                *guard_lhs.to_string() == *(poisoned_rhs.into_inner()).to_string()
+            }
+
+            (Err(poisoned_lhs), Err(poisoned_rhs)) => {
+                *(poisoned_lhs.into_inner()).to_string() == *(poisoned_rhs.into_inner()).to_string()
+            }
+        }
     }
 }
 
